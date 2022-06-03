b7d78a6a28f2cced08c665bea4f3cc5c6c80d817
clarissa2307 <Marthaleo07@gmail.com>
2022-06-03 18:22:13 -0500

    Introducción a las plantillas: Templates

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about/">Acerca de</a></li>
-        <li><a href="/contact/">Contacto</a></li>
-    </ul>
-"""
 
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Bienvenidos</h2>
-        <p>Esto es la portada.</p>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
     return HttpResponse(html_base + """
