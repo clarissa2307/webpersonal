f68e7392345c553fbc0b462d7356b90afd0bd4ad
clarissa2307 <Marthaleo07@gmail.com>
2022-06-03 18:13:41 -0500

    Páginas portafolio y contacto

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -6,6 +6,7 @@ html_base = """
     <ul>
         <li><a href="/">Portada</a></li>
         <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
     </ul>
 """
 
@@ -19,4 +20,15 @@ def about(request):
     return HttpResponse(html_base + """
         <h2>Acerca de</h2>
         <p>Me llamo Héctor y me encanta Django!</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí os dejo mi email y mis redes sociales:</p>
+        <ul>
+            <li><a href="mailto:hola@hektorprofe.net">Email</a></li>
+            <li><a href="https://github.com/hcosta">Github</a></li>
+            <li><a href="https://youtube.com">Youtube</a></li>
+        </ul>
     """)
