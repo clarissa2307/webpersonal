c13f885e6d35d33ab5868f552c590354b46695f3
clarissa2307 <Marthaleo07@gmail.com>
2022-06-03 19:15:17 -0500

    El template tag url

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -23,8 +23,8 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
+    path('about-me/', views.about, name="about"),
     path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
